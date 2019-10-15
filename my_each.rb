f1342663322aa83052380607ea53cee1938b302d
deezy13 <deezy13@gmail.com>
2019-10-15 02:41:23 +0000

    Done.

--- my_each.rb
+++ my_each.rb
@@ -1,3 +1,19 @@
-def my_each # put argument(s) here
-  # code here
+
+
+def my_each(array)
+  i = 0
+  while i < array.length
+# The "while" method will always return a value of => nil"
+    yield(array[i])
+    i = i + 1
+  end
+  array
 end
+
+
+
+# Use the collection method 
+# To make the *return-value* the answer you want
+# instead of "=> nil"
+
+
