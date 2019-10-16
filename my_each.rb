294b6f1955ac9b6aa2418ef6efb164bcf31b73b5
Seyedpouya hajiseyedjavadi <pouya.javadi.resume@gmail.com>
2019-10-16 22:18:07 +0000

    Done.

--- my_each.rb
+++ my_each.rb
@@ -1,3 +1,23 @@
-def my_each # put argument(s) here
-  # code here
+require 'pry'
+
+def my_each(array)
+  if block_given?
+    i = 0 
+    while i < array.length
+      yield array[i]
+      i += 1 
+    end
+    array
+  else
+    return "No block was given"
+  end
 end
+
+my_each([1,2,3,4]) do |number|
+ return number
+end
+
+
+
+
+
