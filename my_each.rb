cd310bc8d230f2cbe1328e39631138918063340c
Tyler Carroll <tylercarroll1611@gmail.com>
2019-10-19 17:59:45 +0000

    Done.

--- my_each.rb
+++ my_each.rb
@@ -1,3 +1,17 @@
-def my_each # put argument(s) here
-  # code here
+collection = []
+
+def my_each(collection)
+  i = 0
+  while i < collection.length 
+  yield collection[i]
+  i += 1 
+  end 
+  collection
 end
+
+
+# my_each(collection) do |i|
+#   i << collection
+#   collection
+# end
+
