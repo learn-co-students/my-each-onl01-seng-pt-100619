715188aac345e8b6b1eb6785b01d629b8ae66834
Abraham Moreno <adiaz2821@gmail.com>
2019-11-12 13:43:58 +0000

    Done.

--- my_each.rb
+++ my_each.rb
@@ -1,3 +1,23 @@
-def my_each # put argument(s) here
-  # code here
-end
+# put argument(s) here
+
+def my_each(people)
+  if block_given?
+  i = 0 
+
+  while i < people.length 
+    yield (people[i])
+    i = i + 1 
+  end 
+
+people 
+  else 
+  return "This"
+  end   
+end 
+
+ # code here
+ 
+people = ["Esther", "Bobby", "Bryan"]
+my_each(people) do |i|
+  return i 
+end 
